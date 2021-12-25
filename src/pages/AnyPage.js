cf578a926873530dd327b52292c5788d08d533fb
Nox-Nox <noxana31@gmail.com>
2021-12-25 18:13:31 +0100

    nothing

---
+++ src/pages/AnyPage.js
@@ -0,0 +1,11 @@
+import FormTemplate from "../components/Form";
+
+function AnyPage() {
+  return (
+    <div>
+      <div>Any Page</div>
+      <FormTemplate />
+    </div>
+  );
+}
+export default AnyPage;
