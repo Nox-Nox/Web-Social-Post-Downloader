7019b93983acd1e4754f4fa4b4243f0d216a2d3e
Nox-Nox <noxana31@gmail.com>
2022-02-01 08:06:05 +0100

    removed the comment section in app.py and made a login to instagram function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from flask_cors import CORS, cross_origin
-from flask import Flask, render_template, request, session, redirect, send_file, url_for, jsonify, Response
+from flask import Flask, render_template, request, Response
 import youtube_dl
 import instaloader
 import requests
@@ -8,10 +8,24 @@ import base64
 from instaloader import Post
 
 app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+
 app.config['SECRET_KEY'] = "DianaWhore99"
 
+
+def login_to_instagram():
+    instance = instaloader.Instaloader(download_geotags=False,
+                                       download_videos=True,
+                                       download_pictures=True,
+                                       download_comments=False,
+                                       download_video_thumbnails=False)
+    instance.post_metadata_txt_pattern = ""
+    instance.download_geotags = False
+    instance.save_metadata = False
+    instance.save_metadata_json = True
+    instance.download_comments = False
+    instance.load_session_from_file("nux7510")
+    print("Logged in")
+    return instance
 
 @app.route('/')
 def home():
@@ -22,22 +36,10 @@ def home():
 @cross_origin()
 def download_instagram():
     if request.method == 'POST':
-        instance = instaloader.Instaloader(download_geotags=False,
-                                           download_videos=True,
-                                           download_pictures=True,
-                                           download_comments=False,
-                                           download_video_thumbnails=False)
-        instance.post_metadata_txt_pattern = ""
-        instance.download_geotags = False
-        instance.save_metadata = False
-        instance.save_metadata_json = True
-        instance.download_comments = False
-        instance.load_session_from_file("nux7510")
-        print("Logged in")
+        instance = login_to_instagram()
+
         original_url = request.get_json()
-
         to_format_url = original_url['url']
-
         short_code = to_format_url.split('p/')[1].split('/')[0]
 
         post = Post.from_shortcode(instance.context, short_code)
@@ -55,25 +57,16 @@ def download_instagram():
                 base64_data = base64.b64encode(q.content)
                 base64_data_string = base64_data.decode("utf-8")
                 data.append({'bytes': base64_data_string, 'title': "video.mp4", 'type': "video/mp4"})
-                # data.append({'url': c, 'title': "video.mp4"})
         if pics:
             for f in pics:
                 r = requests.get(f)
                 base64_data = base64.b64encode(r.content)
                 base64_data_string = base64_data.decode("utf-8")
-                data.append({'bytes': base64_data_string, 'title': "image.jpg", 'type': "image/jpeg", 'type': "image/jpeg"})
+                data.append(
+                    {'bytes': base64_data_string, 'title': "image.jpg", 'type': "image/jpeg", 'type': "image/jpeg"})
 
-            # for t in data:
-            #     print(t)
-            # data.append({'url': f, 'title': "pic.jpg"})
-    # return jsonify(data)
     print(data)
     return json.dumps(data)
-
-
-# return Response(json.dumps(data), status=200, mimetype='application/json')
-# return Response(response = r.reason, status=r.status_code, headers=dict(r.headers))
-# return jsonify(data, 200)
 
 
 @app.route('/download_twitter', methods=['GET', 'POST'])
@@ -89,7 +82,6 @@ def download_twitter():
             data = {'url': info_url['url'], 'title': info_url['title'] + '.mp4'}
 
     return Response(json.dumps(data), status=200, mimetype='video/mp4')
-    # return send_file(buffer, download_name=info_url['title'] + '.mp4')
 
 
 if __name__ == '__main__':
