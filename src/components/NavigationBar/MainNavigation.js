7b509fe60f1bc0efe9cc8a8341547a0e5efeb6cb
Nox-Nox <noxana31@gmail.com>
2022-02-18 01:56:33 +0100

    nothing new

--- src/components/NavigationBar/MainNavigation.js
+++ src/components/NavigationBar/MainNavigation.js
@@ -2,40 +2,31 @@ import { AppBar, Toolbar, Link, ThemeProvider } from "@mui/material";
 import { useEffect, useState } from "react";
 import MainNavigationTheme from "../Themes/MainNavigationTheme";
 
-const theme = MainNavigationTheme;
-const instagramTheme = {
-  background: "linear-gradient(90deg, #F56040, #405DE6)",
-};
-const twitterTheme = { background: "#1DA1F2" };
+const themes = MainNavigationTheme;
+
+const twitterTheme = "#1DA1F2";
+const instagramTheme = "linear-gradient(90deg, #F56040, #405DE6)";
 
 function MainNavigation() {
-  const [isTheme, setTheme] = useState(twitterTheme.background);
-
-  useEffect(() => {
-    const currentTheme = sessionStorage.getItem("theme-color");
-    if (currentTheme) {
-      setTheme(currentTheme);
-    }
-  });
-
+  const [isTheme, setTheme] = useState(twitterTheme);
   const handleClick = (theme) => {
-    setTheme(theme);
-    sessionStorage.setItem("theme-color", theme);
+    setTheme(
+      (themes.components.MuiAppBar.styleOverrides.root.background = theme)
+    );
   };
 
+  useEffect(() => {}, []);
+
   return (
-    <ThemeProvider theme={theme}>
-      <AppBar position="static" style={{ background: isTheme }}>
+    <ThemeProvider theme={themes}>
+      <AppBar position="static">
         <Toolbar>
-          <Link
-            href="/twitterPage"
-            onClick={() => handleClick(twitterTheme.background)}
-          >
+          <Link href="/twitterPage" onClick={() => handleClick(twitterTheme)}>
             Twitter
           </Link>
           <Link
             href="/instagramPage"
-            onClick={() => handleClick(instagramTheme.background)}
+            onClick={() => handleClick(instagramTheme)}
           >
             Instagram
           </Link>
